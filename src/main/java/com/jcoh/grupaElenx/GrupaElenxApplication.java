13ba5a065389e1defdf08f898d488bd0a72670d8
Andriy Androsyuk <andriiandrosiukk@gmail.com>
2018-11-09 13:46:41 +0100

    Initial commit

---
+++ src/main/java/com/jcoh/grupaElenx/GrupaElenxApplication.java
@@ -0,0 +1,12 @@
+package com.jcoh.grupaElenx;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GrupaElenxApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(GrupaElenxApplication.class, args);
+	}
+}
